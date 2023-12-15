cb476031829111a8b11b4da10c5ac02c16d66343
razputshivanshu <89507255+razputshivanshu@users.noreply.github.com>
2023-12-15 10:36:45 +0530

    dsdc

--- Week_2_Backend/index.js
+++ Week_2_Backend/index.js
@@ -92,10 +92,33 @@ function createUser(req,res){
   console.log("User has been created!!!!");
 }
 
-app.post('/handleSum', handleFirstReq);
+function givePage(req,res){
+  res.send(`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning Express</title>
+  </head>
+  <body>
+  
+    <h1>I have to focus more on learning</h1>
+    
+  </body>
+  </html>`);
+
+}
 
 
-// app.post('/handleSum', handleFirstReq);
+
+
+
+app.get('/handleSum', handleFirstReq);
+app.get('/', givePage);
+
+
+
 
 
 function started(){
